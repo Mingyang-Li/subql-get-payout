9299e40321c6b1e0cdb7aa70a4426845c84fcfd3
Mingyang-Li <mingyangli118@gmail.com>
2021-06-30 11:22:48 +1200

    clean up to restart

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,32 +1,5 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {Transfer, Account} from "../types";
-import {Balance} from "@polkadot/types/interfaces";
-
-async function ensureAccounts(accountIds: string[]): Promise<void> {
-    for (const accountId of accountIds) {
-        const account = await Account.get(accountId);
-        if (!account) {
-            await new Account(accountId).save();
-        }
-    }
-}
+import { SubstrateEvent } from "@subql/types";
 
 export async function handleTransfer(event: SubstrateEvent): Promise<void> {
-    const {
-        event: {
-            data: [from, to, amount],
-        },
-    } = event;
-    await ensureAccounts([from.toString(), to.toString()]);
-    const transferInfo = new Transfer(
-        `${event.block.block.header.number.toNumber()}-${event.idx}`,
-    );
-    transferInfo.fromId = from.toString();
-    transferInfo.toId = to.toString();
-    transferInfo.amount = (amount as Balance).toBigInt();
-    await transferInfo.save();
-}
-
-export async function handleEraStakingPayouts(event: SubstrateBlock): Promise<void> {
     return;
 }
