00d6f98fdef2a037d072c406e02b10dd3b7216d7
Mingyang-Li <mingyangli118@gmail.com>
2021-07-05 15:43:55 +1200

    api init placeholder

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -2,6 +2,9 @@ import { SubstrateExtrinsic, SubstrateEvent, SubstrateBlock } from "@subql/types
 import { Balance } from "@polkadot/types/interfaces";
 import { NominatorValidator } from '../types/models/NominatorValidator';
 
+const { ApiPromise, WsProvider } = require(`@polkadot/api`);
+const provider = new WsProvider(`wss://rpc.polkadot.io/`);
+
 async function handleInput(eraNum: number, validatorHash: string): Promise<void> {
     const nominatorValidator = new NominatorValidator("blockid");
     nominatorValidator.era = eraNum;
@@ -10,4 +13,9 @@ async function handleInput(eraNum: number, validatorHash: string): Promise<void>
 
 async function handlePayouts(event: SubstrateEvent): Promise<void> {
     return;
+}
+
+async function apiInit() {
+    const api = await ApiPromise.create({ provider});
+    return api;
 }
