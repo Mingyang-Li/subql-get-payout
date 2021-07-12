fe32ade2d786114b2ca8f4dca9f1cd98e0dabe14
Mingyang-Li <mingyangli118@gmail.com>
2021-07-13 11:34:23 +1200

    cleanup handler

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,30 +1,13 @@
 import { SubstrateExtrinsic, SubstrateEvent, SubstrateBlock } from "@subql/types";
-import { Balance } from "@polkadot/types/interfaces";
+import { EraRewardPoints } from '@polkadot/types/interfaces';
+import { Era } from './../types/models/Era';
 import { NominatorValidator } from '../types/models/NominatorValidator';
+import { ValidatorPayout } from '../types/models/ValidatorPayout';
+import { PayoutDetail } from '../types/models/PayoutDetail';
 
-const { ApiPromise, WsProvider } = require(`@polkadot/api`);
-const provider = new WsProvider(`wss://rpc.polkadot.io/`);
-
-export async function handleInput(eraNum: number, validatorHash: string): Promise<void> {
-    const nominatorValidator = new NominatorValidator("blockid");
-    nominatorValidator.era = eraNum;
-    nominatorValidator.validatorId = validatorHash;
-}
-
-async function handlePayouts(event: SubstrateEvent): Promise<void> {
-    return;
-}
-
-async function apiInit() {
-    const api = await ApiPromise.create({ provider });
-    return api;
-}
-
-
-async function handleTransfers(event: SubstrateEvent): Promise<void> {
-
-    const {accountId, balance }=  event
-
+export async function handleRewards(event: SubstrateEvent, eraNumber: number): Promise<void> {
     const bn = event.block.block.header.number.toBigInt();
+    const totalRewards = (await api.query.staking.erasRewardPoints<EraRewardPoints>(eraNumber));
     // destructure data, manipulate, update entity, send to db, gql will work
+    
 }
