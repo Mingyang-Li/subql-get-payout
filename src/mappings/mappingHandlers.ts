9cfd37b0b679f17cd1aea498af865c619ca6495a
Mingyang-Li <mingyangli118@gmail.com>
2021-07-15 12:06:06 +1200

    focusing on saving Era to db

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,32 +1,51 @@
 import { Era } from './../types/models/Era';
 import { SubstrateExtrinsic, SubstrateEvent, SubstrateBlock } from "@subql/types";
 import { EraRewardPoints } from '@polkadot/types/interfaces';
-import { Account } from './../types/models/Account';
-import { NominatorValidator } from '../types/models/NominatorValidator';
-import { ValidatorPayout } from '../types/models/ValidatorPayout';
-import { PayoutDetail } from '../types/models/PayoutDetail';
+// import { Account } from './../types/models/Account';
+// import { NominatorValidator } from '../types/models/NominatorValidator';
+// import { ValidatorPayout } from '../types/models/ValidatorPayout';
+// import { PayoutDetail } from '../types/models/PayoutDetail';
 
-type EraType = {
-    id: ID! # era idex.
-    startBlock: BigInt!
-    endBlock: BigInt
+export async function handleSession(event:SubstrateEvent) {
+
+    const currentEra = await api.query.staking.currentEra();
+    const currentBlock =  event.block.block.header.number.toBigInt();
+
+    // check it era is obtained. If not, era doesn't exists in db
+    // const eraInDb: boolean = dbEraValue !== null ? true : false;
+    const {
+        event: {
+            data: [SessionIndex]
+        }
+    } = event;
+
+    logger.info(`SessionId: ${SessionIndex.toString()}, EraIndex: ${JSON.stringify(currentEra)}， currentBlock: ${currentBlock.toString()}`);
+    ///
+
+
+    const thisEra = await Era.get(currentEra.toString());
+
+
+
+    if (!thisEra){
+        const newEra: Era = new Era(currentEra.toString());
+        newEra.startBlock = currentBlock;
+        await newEra.save();
+        logger.log(`Create new Era : ${JSON.stringify(newEra)}`)
+        // update endblock of th
+        const previousEraIndex =  Number(currentEra)-1;
+        const previousEra = await Era.get(previousEraIndex.toString());
+
+        if(previousEra){
+            previousEra.endBlock = BigInt(Number(currentBlock) - 1);
+            await previousEra.save();
+            logger.log(`Update prvious Era : ${JSON.stringify(previousEra)}`)
+        }
+
+    }
+
+
+    //Save
+    // check if there's new era in DB, if not, create new era
+
 }
-
-export async function handleRewards(event: SubstrateEvent, eraNumber: number, accountId: string): Promise<void> {
-    const totalRewards = (await api.query.staking.erasRewardPoints<EraRewardPoints>(eraNumber));
-    // get total reward points
-    const totalRewardPoints = parseInt(totalRewards.total.toString());
-    const rewardAmount = await api.query.staking.erasValidatorReward(eraNumber);
-    const totalRewardAmount = EraType(Number(rewardAmount.toString())};
-
-
-    // destructure data, manipulate, update entity, send to db, gql will work
-
-    const account = new Account(accountId);
-    const eraStr = eraNumber.toString(); // to add ids of noms and vals
-    const nomStr = "noIdea";
-    const ValStr = "alsoNoIdeea";
-
-    const nominatorValidator = new NominatorValidator(account.id);
-    const Era =
-    nominatorValidator}
--- src/types/models/index.ts
+++ src/types/models/index.ts
@@ -2,13 +2,5 @@
 
 // Auto-generated , DO NOT EDIT
 
-export {Account} from "./Account"
-
-export {NominatorValidator} from "./NominatorValidator"
-
 export {Era} from "./Era"
-
-export {ValidatorPayout} from "./ValidatorPayout"
-
-export {PayoutDetail} from "./PayoutDetail"
 
