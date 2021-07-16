3e680f8937083cdfab6edbbb103426a0efc43191
Mingyang-Li <mingyangli118@gmail.com>
2021-07-16 16:32:27 +1200

    reordered mapping

--- src/mappings/handleSession.ts
+++ src/mappings/handleSession.ts
@@ -5,7 +5,7 @@ import { ValidatorPayout } from '../types/models/ValidatorPayout';
 import { PayoutDetail } from '../types/models/PayoutDetail';
 import { sha256 } from 'js-sha256';
 
-export async function handleSession(event:SubstrateEvent) {
+export async function handleSession(event:SubstrateEvent): Promise<void> {
     const currentEra = await api.query.staking.currentEra();
     const currentBlock =  event.block.block.header.number.toBigInt();
     // check it era is obtained. If not, era doesn't exists in db
@@ -21,40 +21,7 @@ export async function handleSession(event:SubstrateEvent) {
     }
 
     const currentEraNum = currentEra.unwrap().toNumber();
-    const thisEra = currentEra.toString();
-    
-    const validators = await api.query.session.validators();
-    for (const validator of validators){
-        // logger.info(`----- validator: ${validator.toString()} at Era: ${currentEraNum}:`);
-        const validatorExposure =  await api.query.staking.erasStakers(currentEraNum, validator.toString());
-        const { total, own, others } = validatorExposure;
-        others.forEach(nominator => {
-            // To create NominatorValidator, first we need to create a compisite ID based off its 3 attributes
-            const nominatorValidatorId: string = sha256(`${currentEraNum}${nominator.who.toString()}${validator.toString()}`);
-
-            // Once hashed ID is created, we can populate NominatorValidator
-            const currNominatorValidator = new NominatorValidator(nominatorValidatorId);
-            currNominatorValidator.eraId = currentEraNum.toString();
-            currNominatorValidator.nominatorId = nominator.who.toString();
-            currNominatorValidator.validatorId = validator.toString();
-            currNominatorValidator.save(); //Save to DB
-        });
-
-        // Populate ValidatorPayout
-        const payoutRewards = await api.query.staking.erasValidatorReward(currentEraNum);
-        const currValidatorPayout = new ValidatorPayout(sha256(`${currentEraNum.toString()}${validator.toString()}`));
-        currValidatorPayout.eraId = currentEraNum.toString();
-        currValidatorPayout.eraPayout = BigInt(payoutRewards.toString());
-        currValidatorPayout.claimed = false; // placeholder, working it out
-
-        // Populate PayoutDetail
-        const currPayoutDetail = new PayoutDetail(sha256(`${currentEraNum.toString()}${validator.toString()}`));
-        currPayoutDetail.eraId = currentEraNum.toString();
-        currPayoutDetail.accountId = validator.toString();
-        currPayoutDetail.claimed = false; // placeholder
-        currPayoutDetail.payoutId = currValidatorPayout.id;
-        currValidatorPayout.save();
-    }
+    const thisEra = await Era.get(currentEraNum.toString());
 
     // create new era (when not found in db)
     if (!thisEra){
@@ -71,5 +38,40 @@ export async function handleSession(event:SubstrateEvent) {
             await previousEra.save();
         }
     }
-}
+    
+    const validators = await api.query.session.validators();
+    for (const validator of validators){
+        // logger.info(`----- validator: ${validator.toString()} at Era: ${currentEraNum}:`);
+        const validatorExposure =  await api.query.staking.erasStakers(currentEraNum, validator.toString());
+        const { total, own, others } = validatorExposure;
 
+        for (const nominator of others) {
+            // To create NominatorValidator, first we need to create a compisite ID based off its 3 attributes
+            const nominatorValidatorId: string = sha256(`${currentEraNum}${nominator.who.toString()}${validator.toString()}`);
+
+            // Once hashed ID is created, we can populate NominatorValidator
+            const currNominatorValidator = new NominatorValidator(nominatorValidatorId);
+            currNominatorValidator.eraId = currentEraNum.toString();
+            currNominatorValidator.nominatorId = nominator.who.toString();
+            currNominatorValidator.validatorId = validator.toString();
+            await currNominatorValidator.save();
+        }
+
+        // Populate ValidatorPayout
+        const payoutRewards = await api.query.staking.erasValidatorReward(currentEraNum);
+        const currValidatorPayout = new ValidatorPayout(sha256(`${currentEraNum.toString()}${validator.toString()}`));
+        currValidatorPayout.eraId = currentEraNum.toString();
+        currValidatorPayout.eraPayout = BigInt(payoutRewards.toString());
+        currValidatorPayout.claimed = false; // placeholder, working it out
+        currValidatorPayout.claimedAtBlock = currentBlock;
+        await currValidatorPayout.save();
+
+        // Populate PayoutDetail
+        const currPayoutDetail = new PayoutDetail(sha256(`${currentEraNum.toString()}${validator.toString()}`));
+        currPayoutDetail.eraId = currentEraNum.toString();
+        currPayoutDetail.accountId = validator.toString();
+        currPayoutDetail.claimed = false; // placeholder
+        currPayoutDetail.payoutId = currValidatorPayout.id;
+        await currValidatorPayout.save();
+    }
+}
